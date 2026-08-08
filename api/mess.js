feefcf7f5863f06ecac7b101d703b2d744e6848f
xyron-02 <xyronbywanz@gmail.com>
2026-08-08 17:25:11 +0700

    Delete api/mess.js

--- api/mess.js
+++
@@ -1,9 +0,0 @@
-// api/mess.js
-export default function handler(req, res) {
-  res.setHeader('Content-Type', 'application/json');
-  res.status(200).json([ 
-    'iya sayangkuu iyaa',
-    'kam si cantek na sayang yah',
-    'ula kam gutul yahh'
-  ]);
-}
