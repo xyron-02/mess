10c0b4e4b1aa94f8969778b0a1e8f56d2d06281a
xyron-02 <xyronbywanz@gmail.com>
2026-08-08 18:13:28 +0700

    Update mess-1.js

--- api/mess-1.js
+++ api/mess-1.js
@@ -1,12 +1,7 @@
 export default function handler(req, res) {
-  const data = [
-    'iya sayangggg',
-    '', 
-    '© cowo kuin'
-  ];
   
   res.setHeader('Content-Type', 'application/json');
   res.status(200).send(JSON.stringify({
-   data
+   'iya sayangggg'
   }, null, 2));
 }
